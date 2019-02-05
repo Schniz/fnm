2b7af929cfeb834d0489195433d518ec8a6851a5
Gal Schlezinger <gal@spitfire.co.il>
2019-02-05 17:29:50 +0200

    moar text

--- .ci/install.sh
+++ .ci/install.sh
@@ -27,8 +27,8 @@ download_fnm() {
   echo "Downloading $URL..."
 
   mkdir -p $HOME/.fnm &> /dev/null
-  curl -L $URL -o $DOWNLOAD_DIR/$FILENAME.zip
-  unzip $DOWNLOAD_DIR/$FILENAME.zip -d $DOWNLOAD_DIR
+  curl --progress-bar -L $URL -o $DOWNLOAD_DIR/$FILENAME.zip
+  unzip -q $DOWNLOAD_DIR/$FILENAME.zip -d $DOWNLOAD_DIR
   mv $DOWNLOAD_DIR/$FILENAME/fnm $HOME/.fnm/fnm
   chmod u+x $HOME/.fnm/fnm
 }
@@ -37,33 +37,53 @@ setup_shell() {
   CURRENT_SHELL=$(basename $SHELL)
 
   if [ "$CURRENT_SHELL" == "zsh" ]; then
-    echo "Installing for Zsh. Appending the following to $HOME/.zshrc:"
-    echo 'export PATH=$HOME/.fnm:$PATH'
-    echo 'eval `fnm env`'
+    CONF_FILE=$HOME/.zshrc
+    echo "Installing for Zsh. Appending the following to $CONF_FILE:"
+    echo ""
+    echo '  # fnm'
+    echo '  export PATH=$HOME/.fnm:$PATH'
+    echo '  eval `fnm env`'
 
-    echo 'export PATH=$HOME/.fnm:$PATH' >> $HOME/.zshrc
-    echo 'eval `fnm env`' >> $HOME/.zshrc
+    echo '' >> $CONF_FILE
+    echo '# fnm' >> $CONF_FILE
+    echo 'export PATH=$HOME/.fnm:$PATH' >> $CONF_FILE
+    echo 'eval `fnm env`' >> $CONF_FILE
 
   elif [ "$CURRENT_SHELL" == "fish" ]; then
-    echo "Installing for Fish. Appending the following to $HOME/.config/fish/config.fish:"
-    echo 'set PATH $HOME/.fnm $PATH'
-    echo 'eval (fnm env --fish)'
+    CONF_FILE=$HOME/.config/fish/config.fish
+    echo "Installing for Fish. Appending the following to $CONF_FILE:"
+    echo ""
+    echo '  # fnm'
+    echo '  set PATH $HOME/.fnm $PATH'
+    echo '  eval (fnm env --fish)'
 
-    echo 'set PATH $HOME/.fnm $PATH' >> $HOME/.config/fish/config.fish
-    echo 'eval (fnm env --fish)' >> $HOME/.config/fish/config.fish
+    echo '' >> $CONF_FILE
+    echo '# fnm' >> $CONF_FILE
+    echo 'set PATH $HOME/.fnm $PATH' >> $CONF_FILE
+    echo 'eval (fnm env --fish)' >> $CONF_FILE
 
   elif [ "$CURRENT_SHELL" == "bash" ]; then
-    echo "Installing for Bash. Appending the following to $HOME/.bashrc:"
-    echo 'export PATH=$HOME/.fnm:$PATH'
-    echo 'eval `fnm env`'
+    CONF_FILE=$HOME/.bashrc
+    echo "Installing for Bash. Appending the following to $CONF_FILE:"
+    echo ""
+    echo '  # fnm'
+    echo '  export PATH=$HOME/.fnm:$PATH'
+    echo '  eval `fnm env`'
 
-    echo 'export PATH=$HOME/.fnm:$PATH' >> $HOME/.bashrc
-    echo 'eval `fnm env`' >> $HOME/.bashrc
+    echo '' >> $CONF_FILE
+    echo '# fnm' >> $CONF_FILE
+    echo 'export PATH=$HOME/.fnm:$PATH' >> $CONF_FILE
+    echo 'eval `fnm env`' >> $CONF_FILE
 
   else
     echo "Could not infer shell type. Please set up manually."
     exit 1
   fi
+
+  echo ""
+  echo "In order to apply the changes, open a new terminal or run the following command:"
+  echo ""
+  echo "  source $CONF_FILE"
 }
 
 download_fnm
