9be357bd94594ab53e85fdc1ca7276c427c8a7ac
Gal Schlezinger <gal@spitfire.co.il>
2019-02-10 22:44:53 +0200

    fnm-linux => fnm. Fixes #24 (#25)

--- .ci/create-static-binary.sh
+++ .ci/create-static-binary.sh
@@ -4,6 +4,6 @@ echo "Building binary in docker"
 
 docker build . -t schlez/fnm-static-binary
 
-echo "Copying to ./fnm-linux"
+echo "Copying to ./fnm"
 
-docker run --rm -v $(pwd):$(pwd) --workdir $(pwd) schlez/fnm-static-binary cp /app/_build/default/executable/FnmApp.exe ./fnm-linux
+docker run --rm -v $(pwd):$(pwd) --workdir $(pwd) schlez/fnm-static-binary cp /app/_build/default/executable/FnmApp.exe ./fnm
