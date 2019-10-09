ca43c9f1e7c04c9112c18891fd9c5c42f9c612e5
James Thomson <thomsj@users.noreply.github.com>
2019-10-09 21:48:19 +0100

    Add missing `exit 1`s to `partial_semver` tests

--- feature_tests/partial_semver/run.sh
+++ feature_tests/partial_semver/run.sh
@@ -10,9 +10,11 @@ fnm install 8.11.3
 fnm use 6
 if [ "$(node -v)" != "v6.17.1" ]; then
   echo "Node version mismatch: $(node -v). Expected: v6.17.1"
+  exit 1
 fi
 
 fnm use 8
 if [ "$(node -v)" != "v8.11.3" ]; then
   echo "Node version mismatch: $(node -v). Expected: v8.11.3"
+  exit 1
 fi
