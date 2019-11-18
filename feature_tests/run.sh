eaa154b63829985fa737fa894663850157691d62
Gal Schlezinger <gal@spitfire.co.il>
2019-11-18 12:09:39 +0200

    Support spaces in directory name in Bash. Fixes #168

--- feature_tests/run.sh
+++ feature_tests/run.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-DIRECTORY=`dirname $0`
-BINARY=$1
+DIRECTORY="$(dirname "$0")"
+BINARY="$1"
+SPECIFIC_TEST="$2"
 TEMP_DIR_BASE=$(pwd)/$DIRECTORY/.tmp
 TEMP_BINARY_PATH=$TEMP_DIR_BASE/bin
 TEMP_FNM_DIR=$TEMP_DIR_BASE/.fnm
@@ -15,18 +16,30 @@ fi
 
 echo "using fnm=$BINARY"
 
-rm -rf $TEMP_DIR_BASE
-mkdir $TEMP_DIR_BASE $TEMP_BINARY_PATH
-cp $BINARY $TEMP_BINARY_PATH/fnm
+rm -rf "$TEMP_DIR_BASE"
+mkdir "$TEMP_DIR_BASE" "$TEMP_BINARY_PATH"
+cp "$BINARY" "$TEMP_BINARY_PATH/fnm"
 
-for test_file in $DIRECTORY/*/run.sh; do
-  rm -rf $TEMP_FNM_DIR
+run_test() {
+  test_file="$1"
+  rm -rf "$TEMP_FNM_DIR"
+
+  TEST_BASENAME="$(basename "$test_file")"
+  TEST_DIRNAME="$(dirname "$test_file")"
 
   echo "Running test in $test_file"
   echo "Running test in $test_file" | sed "s/./-/g"
-  (cd $(dirname $test_file) && FNM_DIR=$TEMP_FNM_DIR PATH=$TEMP_BINARY_PATH:$PATH bash $(basename $test_file))
+  (cd "$TEST_DIRNAME" && FNM_DIR="$TEMP_FNM_DIR" PATH="$TEMP_BINARY_PATH:$PATH" bash "$TEST_BASENAME")
   echo ""
   echo " -> Finished!"
 
-  rm -rf $TEMP_FNM_DIR
-done
+  rm -rf "$TEMP_FNM_DIR"
+}
+
+if [ "$SPECIFIC_TEST" == "" ]; then
+  for test_file in "$DIRECTORY"/*/run.sh; do
+    run_test "$test_file"
+  done
+else
+  run_test "$SPECIFIC_TEST"
+fi
