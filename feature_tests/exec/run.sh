97d454c130dbb1d634937253ee2b9f5acc38caae
Gal Schlezinger <gal@spitfire.co.il>
2020-02-26 16:26:47 +0200

    ensure in tests that a specific version isn't necessary

--- feature_tests/exec/run.sh
+++ feature_tests/exec/run.sh
@@ -3,7 +3,10 @@
 set -e
 
 fnm install v6.10.0
+fnm install v8.10.0
 fnm install v10.10.0
+fnm use v8.10.0
 
+fnm exec -- node -v | grep "v8.10.0"
 fnm exec --using 6 -- node -v | grep "v6.10.0"
 fnm exec --using 10 -- node -v | grep "v10.10.0"
