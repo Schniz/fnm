438c5878d5de674cb5d21f0ee730890072e7eead
Amit Dahan <amitda@wix.com>
2020-03-27 19:31:17 +0300

    Add almost-matching-dotfiles test case

---
+++ feature_tests/almost-matching-dotfiles/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#set -e
+
+eval $(fnm env)
+fnm install
+fnm use
+
+echo node --version
+if [ "$(node --version)" != "v11.10.0" ]; then
+  echo "Expected Node version is not v11.10.0!"
+  exit 1
+fi
