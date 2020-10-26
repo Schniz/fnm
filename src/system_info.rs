b8567020be7fd12411f8c91ff80b791c193ec36c
Gal Schlezinger <gal@spitfire.co.il>
2020-10-26 13:00:14 +0200

    Add pre-built binaries for ARM32 and ARM64 (#276)

--- src/system_info.rs
+++ src/system_info.rs
@@ -8,22 +8,34 @@ pub fn platform_name() -> &'static str {
     "linux"
 }
 
-#[cfg(all(target_pointer_width = "32", target_arch = "arm"))]
+#[cfg(all(
+    target_pointer_width = "32",
+    any(target_arch = "arm", target_arch = "aarch64")
+))]
 pub fn platform_arch() -> &'static str {
     "armv7l"
 }
 
-#[cfg(all(target_pointer_width = "32", not(target_arch = "arm")))]
+#[cfg(all(
+    target_pointer_width = "32",
+    not(any(target_arch = "arm", target_arch = "aarch64"))
+))]
 pub fn platform_arch() -> &'static str {
     "x86"
 }
 
-#[cfg(all(target_pointer_width = "64", target_arch = "arm"))]
+#[cfg(all(
+    target_pointer_width = "64",
+    any(target_arch = "arm", target_arch = "aarch64")
+))]
 pub fn platform_arch() -> &'static str {
     "arm64"
 }
 
-#[cfg(all(target_pointer_width = "64", not(target_arch = "arm")))]
+#[cfg(all(
+    target_pointer_width = "64",
+    not(any(target_arch = "arm", target_arch = "aarch64"))
+))]
 pub fn platform_arch() -> &'static str {
     "x64"
 }
