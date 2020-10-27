d5bab7e1dbf61483d1f8b4a8d70a7c7b71b76717
Max Jacobson <max@hardscrabble.net>
2020-10-27 04:03:51 -0400

    Remove logs from automatic version switching in `--use-on-cd` (#289)

--- src/shell/fish.rs
+++ src/shell/fish.rs
@@ -24,10 +24,8 @@ impl Shell for Fish {
                 function _fnm_autoload_hook --on-variable PWD --description 'Change Node version on directory change'
                     status --is-command-substitution; and return
                     if test -f .node-version
-                        echo "fnm: Found .node-version"
                         fnm use
                     else if test -f .nvmrc
-                        echo "fnm: Found .nvmrc"
                         fnm use
                     end
                 end
--- src/shell/zsh.rs
+++ src/shell/zsh.rs
@@ -24,10 +24,8 @@ impl Shell for Zsh {
                 autoload -U add-zsh-hook
                 _fnm_autoload_hook () {
                     if [[ -f .node-version && -r .node-version ]]; then
-                        echo "fnm: Found .node-version"
                         fnm use
                     elif [[ -f .nvmrc && -r .nvmrc ]]; then
-                        echo "fnm: Found .nvmrc"
                         fnm use
                     fi
                 }
