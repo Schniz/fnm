92daae044f1453549f8a811439850701bdb9f90b
Gal Schlezinger <gal@spitfire.co.il>
2021-04-14 11:17:49 +0300

    update

--- .ci/print-command-docs.js
+++ .ci/print-command-docs.js
@@ -5,11 +5,37 @@
 const execa = require("execa");
 const path = require("path");
 const fs = require("fs");
+const cmd = require("cmd-ts");
 
-main();
+const command = cmd.command({
+  name: "print-command-docs",
+  description: "prints the docs/command.md file with updated contents",
+  args: {
+    checkForDirty: cmd.flag({
+      long: "check",
+      description: `Check that file was not changed`,
+    }),
+  },
+  async handler({ checkForDirty }) {
+    const targetFile = path.join(__dirname, "../docs/commands.md");
+    await main(targetFile);
+    if (checkForDirty && (await checkGitStatus(targetFile)) === "dirty") {
+      const command = "`yarn generate-command-docs`";
+      throw new Error(`The file has changed. Please re-run ${command}`);
+    }
+  },
+});
 
-async function main() {
-  const targetFile = path.join(__dirname, "../docs/commands.md");
+cmd.run(cmd.binary(command), process.argv).catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
+
+/**
+ * @param {string} targetFile
+ * @returns {Promise<void>}
+ */
+async function main(targetFile) {
   const stream = fs.createWriteStream(targetFile);
 
   const { subcommands, text: mainText } = await getCommandHelp();
@@ -78,6 +104,15 @@ function run(args) {
   });
 }
 
-async function ensureNoChangedFilesInGit() {
-  execa("git", ["status"]);
+/**
+ * @param {string} targetFile
+ * @returns {Promise<"dirty" | "clean">}
+ */
+async function checkGitStatus(targetFile) {
+  try {
+    await execa(`git`, ["diff", "--quiet", targetFile]);
+    return "clean";
+  } catch (e) {
+    return "dirty";
+  }
 }
