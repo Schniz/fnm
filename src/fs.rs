3f700ef00b4ecc9d289aa98541d07537fc1e74e3
Gal Schlezinger <gal@spitfire.co.il>
2021-10-17 16:13:05 +0300

    Fix Windows build

--- src/fs.rs
+++ src/fs.rs
@@ -24,12 +24,6 @@ pub fn remove_symlink_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
     Ok(())
 }
 
-#[cfg(unix)]
 pub fn shallow_read_symlink<P: AsRef<Path>>(path: P) -> std::io::Result<std::path::PathBuf> {
     std::fs::read_link(path)
-}
-
-#[cfg(windows)]
-pub fn shallow_read_symlink<P: AsRef<Path>>(path: P) -> std::io::Result<std::path::PathBuf> {
-    junction::get_target(path)
 }
