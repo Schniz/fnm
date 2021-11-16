8bd33dc1b94f79a57c887d7bd4368c8a706f3259
Gal Schlezinger <gal@spitfire.co.il>
2021-11-17 00:58:42 +0200

    First fix for slowess in fnm env (#576)

--- src/shell/infer/mod.rs
+++ src/shell/infer/mod.rs
@@ -10,12 +10,12 @@ struct ProcessInfo {
 }
 
 pub fn infer_shell() -> Option<Box<dyn Shell>> {
-    let system = System::new_all();
-    let hashmap = system.processes();
+    let mut system = System::new();
     let mut current_pid = sysinfo::get_current_pid().ok();
 
     while let Some(pid) = current_pid {
-        if let Some(process) = hashmap.get(&pid) {
+        system.refresh_process(pid);
+        if let Some(process) = system.process(pid) {
             current_pid = process.parent();
             let process_name = process
                 .exe()
