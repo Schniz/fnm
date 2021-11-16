2f31cb04bc4a1ba48cead6e2ecfd45273d3d26df
Gal Schlezinger <gal@spitfire.co.il>
2021-11-16 12:30:45 +0200

    Try to infer shell with sysinfo crate
    This should remove complexity in Windows and Unix implementation by having the same implementation for both.
    The only thing I am now worried (if this will actually pass the tests), is that `cmd` sounds like a fairly common executable name.

--- src/shell/infer/mod.rs
+++ src/shell/infer/mod.rs
@@ -1,11 +1,39 @@
-#[cfg(unix)]
-pub mod unix;
-
-#[cfg(windows)]
-pub mod windows;
+use super::{Bash, Fish, PowerShell, Shell, WindowsCmd, Zsh};
+use log::debug;
+use sysinfo::{ProcessExt, System, SystemExt};
 
 #[derive(Debug)]
 struct ProcessInfo {
     parent_pid: Option<u32>,
     command: String,
+}
+
+pub fn infer_shell() -> Option<Box<dyn Shell>> {
+    let system = System::new_all();
+    let hashmap = system.processes();
+    let mut current_pid = sysinfo::get_current_pid().ok();
+
+    while let Some(pid) = current_pid {
+        if let Some(process) = hashmap.get(&pid) {
+            current_pid = process.parent();
+            let process_name = process
+                .exe()
+                .file_stem()
+                .and_then(|x| x.to_str())
+                .map(|x| x.to_lowercase());
+            let sliced = process_name.as_ref().map(|x| &x[..]);
+            match sliced {
+                Some("sh" | "bash") => return Some(Box::from(Bash)),
+                Some("zsh") => return Some(Box::from(Zsh)),
+                Some("fish") => return Some(Box::from(Fish)),
+                Some("pwsh" | "powershell") => return Some(Box::from(PowerShell)),
+                Some("cmd") => return Some(Box::from(WindowsCmd)),
+                cmd_name => debug!("binary is not a supported shell: {:?}", cmd_name),
+            };
+        } else {
+            current_pid = None;
+        }
+    }
+
+    None
 }
--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -10,17 +10,8 @@ mod shell;
 
 pub use bash::Bash;
 pub use fish::Fish;
+pub use infer::infer_shell;
 pub use powershell::PowerShell;
 pub use shell::{Shell, AVAILABLE_SHELLS};
 pub use windows_cmd::WindowsCmd;
 pub use zsh::Zsh;
-
-#[cfg(windows)]
-pub fn infer_shell() -> Option<Box<dyn Shell>> {
-    self::infer::windows::infer_shell()
-}
-
-#[cfg(unix)]
-pub fn infer_shell() -> Option<Box<dyn Shell>> {
-    infer::unix::infer_shell()
-}
