356aa1f4b30f87f78c8d81274fe1aefead22d283
Gal Schlezinger <gal@spitfire.co.il>
2022-01-10 16:33:31 +0200

    cargo clippy --fix

--- src/choose_version_for_user_input.rs
+++ src/choose_version_for_user_input.rs
@@ -54,18 +54,16 @@ pub fn choose_version_for_user_input<'a>(
                 path: alias_path,
                 version: Version::Bypassed,
             })
+        } else if !alias_path.exists() {
+            return Err(Error::CantFindVersion {
+                requested_version: requested_version.clone(),
+            });
         } else {
-            if !alias_path.exists() {
-                return Err(Error::CantFindVersion {
-                    requested_version: requested_version.clone(),
-                });
-            } else {
-                info!("Using Node for alias {}", alias_name.cyan());
-                Some(ApplicableVersion {
-                    path: alias_path,
-                    version: Version::Alias(alias_name),
-                })
-            }
+            info!("Using Node for alias {}", alias_name.cyan());
+            Some(ApplicableVersion {
+                path: alias_path,
+                version: Version::Alias(alias_name),
+            })
         }
     } else {
         let current_version = requested_version.to_version(&all_versions, config);
