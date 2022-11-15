c4a31f84d744a877266ee1ed1b1ab117096f5d3f
Gal Schlezinger <gal@spitfire.co.il>
2022-11-15 18:52:09 +0200

    use shell-escape

--- e2e/shellcode/shells/sub-shell.ts
+++ e2e/shellcode/shells/sub-shell.ts
@@ -1,17 +1,17 @@
 import { ScriptLine, define } from "./types"
-import { quote } from "shescape"
+import quote from "shell-escape"
 
 type HasInSubShell = { inSubShell: (script: ScriptLine) => ScriptLine }
 
 export const cmdInSubShell = {
   bash: define<HasInSubShell>({
-    inSubShell: (script) => `echo ${quote(script)} | bash`,
+    inSubShell: (script) => `echo ${quote([script])} | bash`,
   }),
   zsh: define<HasInSubShell>({
-    inSubShell: (script) => `echo ${quote(script)} | zsh`,
+    inSubShell: (script) => `echo ${quote([script])} | zsh`,
   }),
   fish: define<HasInSubShell>({
-    inSubShell: (script) => `fish -c ${quote(script)}`,
+    inSubShell: (script) => `fish -c ${quote([script])}`,
   }),
   powershell: define<HasInSubShell>({
     inSubShell: (script) =>
