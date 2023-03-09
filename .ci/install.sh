cbc2a10b3313ebd76b13f98345f3a3fcbe8100a1
Emilio Vesprini <emivespa1@gmail.com>
2023-03-09 01:57:53 +0000

    Make install.sh tee instead of echoing twice
    Also, fish bit was the only one that didn't print a newline to
    $CONF_FILE, now they're all the same.

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,17 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 RELEASE="latest"
 OS="$(uname -s)"
-USE_HOMEBREW=""
-FORCE_INSTALL=""
-SHOULD_EXIT=""
-SKIP_SHELL=""
-
-case "${OS}" in
-   MINGW* | Win*) OS="Windows" ;;
-esac
 
 if [ -d "$HOME/.fnm" ]; then
   INSTALL_DIR="$HOME/.fnm"
@@ -25,7 +17,7 @@ fi
 
 # Parse Flags
 parse_args() {
-  while [ "$#" -gt 0 ]; do
+  while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
@@ -78,9 +70,6 @@ set_filename() {
   elif [ "$OS" = "Darwin" ]; then
     USE_HOMEBREW="true"
     echo "Downloading fnm using Homebrew..."
-  elif [ "$OS" = "Windows" ] ; then
-    FILENAME="fnm-windows"
-    echo "Downloading the latest fnm binary from GitHub..."
   else
     echo "OS $OS is not supported."
     echo "If you think that's a bug - please file an issue to https://github.com/Schniz/fnm/issues"
@@ -88,44 +77,9 @@ set_filename() {
   fi
 }
 
-# Portable temp dir creator (uses mktemp if available, with a fallback)
-mktempdir() {
-  # Prefer mktemp if available; try common variants
-  if command -v mktemp >/dev/null 2>&1; then
-    if tmp=$(mktemp -d "${TMPDIR:-/tmp}/fnm.XXXXXXXX" 2>/dev/null); then
-      printf '%s\n' "$tmp"
-      return 0
-    fi
-    if tmp=$(TMPDIR="${TMPDIR:-/tmp}" mktemp -d -t fnm 2>/dev/null); then
-      printf '%s\n' "$tmp"
-      return 0
-    fi
-    if tmp=$(mktemp -d 2>/dev/null); then
-      printf '%s\n' "$tmp"
-      return 0
-    fi
-  fi
-
-  # Fallback: predictable, but private-perms directory creation loop
-  base="${TMPDIR:-/tmp}/fnm.$$"
-  i=0
-  while [ "$i" -lt 1000 ]; do
-    dir="${base}.${i}"
-    if (umask 077; mkdir "$dir" 2>/dev/null); then
-      printf '%s\n' "$dir"
-      return 0
-    fi
-    i=$((i + 1))
-  done
-
-  printf '%s\n' "mktempdir: could not create a temp directory under ${TMPDIR:-/tmp}" >&2
-  return 1
-}
-
 download_fnm() {
   if [ "$USE_HOMEBREW" = "true" ]; then
     brew install fnm
-    INSTALL_DIR="$(brew --prefix fnm)/bin"
   else
     if [ "$RELEASE" = "latest" ]; then
       URL="https://github.com/Schniz/fnm/releases/latest/download/$FILENAME.zip"
@@ -133,14 +87,11 @@ download_fnm() {
       URL="https://github.com/Schniz/fnm/releases/download/$RELEASE/$FILENAME.zip"
     fi
 
-    DOWNLOAD_DIR=$(mktempdir) || { echo "Unable to create a temporary directory under ${TMPDIR:-/tmp}."; exit 1; }
-    trap "rm -rf '$DOWNLOAD_DIR'" EXIT INT HUP TERM
-
-    echo "Installing fnm to $INSTALL_DIR"
-
-    mkdir -p "$INSTALL_DIR" >/dev/null 2>&1
+    DOWNLOAD_DIR=$(mktemp -d)
 
     echo "Downloading $URL..."
+
+    mkdir -p "$INSTALL_DIR" &>/dev/null
 
     if ! curl --progress-bar --fail -L "$URL" -o "$DOWNLOAD_DIR/$FILENAME.zip"; then
       echo "Download failed.  Check that the release/filename are correct."
@@ -162,16 +113,16 @@ download_fnm() {
 check_dependencies() {
   echo "Checking dependencies for the installation script..."
 
-  printf "Checking availability of curl... "
-  if command -v curl >/dev/null 2>&1; then
+  echo -n "Checking availability of curl... "
+  if hash curl 2>/dev/null; then
     echo "OK!"
   else
     echo "Missing!"
     SHOULD_EXIT="true"
   fi
 
-  printf "Checking availability of unzip... "
-  if command -v unzip >/dev/null 2>&1; then
+  echo -n "Checking availability of unzip... "
+  if hash unzip 2>/dev/null; then
     echo "OK!"
   else
     echo "Missing!"
@@ -179,8 +130,8 @@ check_dependencies() {
   fi
 
   if [ "$USE_HOMEBREW" = "true" ]; then
-    printf "Checking availability of Homebrew (brew)... "
-    if command -v brew >/dev/null 2>&1; then
+    echo -n "Checking availability of Homebrew (brew)... "
+    if hash brew 2>/dev/null; then
       echo "OK!"
     else
       echo "Missing!"
@@ -195,10 +146,11 @@ check_dependencies() {
 }
 
 ensure_containing_dir_exists() {
-  containing_dir="$(dirname "$1")"
-  if [ ! -d "$containing_dir" ]; then
-    echo " >> Creating directory $containing_dir"
-    mkdir -p "$containing_dir"
+  local CONTAINING_DIR
+  CONTAINING_DIR="$(dirname "$1")"
+  if [ ! -d "$CONTAINING_DIR" ]; then
+    echo " >> Creating directory $CONTAINING_DIR"
+    mkdir -p "$CONTAINING_DIR"
   fi
 }
 
@@ -212,13 +164,8 @@ setup_shell() {
     {
       echo ''
       echo '# fnm'
-      echo 'FNM_PATH="'"$INSTALL_DIR"'"'
-      echo 'if [ -d "$FNM_PATH" ]; then'
-      if [ "$USE_HOMEBREW" != "true" ]; then
-        echo '  export PATH="$FNM_PATH:$PATH"'
-      fi
-      echo '  eval "$(fnm env)"'
-      echo 'fi'
+      echo 'export PATH="'$INSTALL_DIR':$PATH"'
+      echo 'eval "`fnm env`"'
     } | tee -a "$CONF_FILE"
 
   elif [ "$CURRENT_SHELL" = "fish" ]; then
@@ -228,13 +175,8 @@ setup_shell() {
     {
       echo ''
       echo '# fnm'
-      echo 'set FNM_PATH "'"$INSTALL_DIR"'"'
-      echo 'if [ -d "$FNM_PATH" ]'
-      if [ "$USE_HOMEBREW" != "true" ]; then
-        echo '  set PATH "$FNM_PATH" $PATH'
-      fi
-      echo '  fnm env | source'
-      echo 'end'
+      echo 'set PATH "'"$INSTALL_DIR"'" $PATH'
+      echo 'fnm env | source'
     } | tee -a "$CONF_FILE"
 
   elif [ "$CURRENT_SHELL" = "bash" ]; then
@@ -248,13 +190,8 @@ setup_shell() {
     {
       echo ''
       echo '# fnm'
-      echo 'FNM_PATH="'"$INSTALL_DIR"'"'
-      echo 'if [ -d "$FNM_PATH" ]; then'
-      if [ "$USE_HOMEBREW" != "true" ]; then
-        echo '  export PATH="$FNM_PATH:$PATH"'
-      fi
-      echo '  eval "$(fnm env)"'
-      echo 'fi'
+      echo 'export PATH="'"$INSTALL_DIR"':$PATH"'
+      echo 'eval "`fnm env`"'
     } | tee -a "$CONF_FILE"
 
   else
@@ -265,11 +202,7 @@ setup_shell() {
   echo ""
   echo "In order to apply the changes, open a new terminal or run the following command:"
   echo ""
-  if [ "$CURRENT_SHELL" = "fish" ]; then
-    echo "  source $CONF_FILE"
-  else
-    echo "  . $CONF_FILE"
-  fi
+  echo "  source $CONF_FILE"
 }
 
 parse_args "$@"
