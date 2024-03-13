664b1207bdbb4bd646d19310196e1be04508bffd
Tobias Tschinkowitz <tobias@truehumandesign.net>
2024-03-13 08:15:22 +0100

    fix: fish shell path duplicates

--- src/shell/fish.rs
+++ src/shell/fish.rs
@@ -18,7 +18,7 @@ impl Shell for Fish {
             .ok_or_else(|| anyhow::anyhow!("Can't convert path to string"))?;
         let path =
             super::windows_compat::maybe_fix_windows_path(path).unwrap_or_else(|| path.to_string());
-        Ok(format!("set -gx PATH {path:?} $PATH;"))
+        Ok(format!("fish_add_path -gP {path:?};"))
     }
 
     fn set_env_var(&self, name: &str, value: &str) -> String {
