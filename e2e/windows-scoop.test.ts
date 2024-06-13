2b32d947ac61d594e04fd9cd7a930edd0119da34
Gal Schlezinger <gal@spitfire.co.il>
2024-06-13 12:30:26 +0300

    test scoop shims

---
+++ e2e/windows-scoop.test.ts
@@ -0,0 +1,32 @@
+import { script } from "./shellcode/script.js"
+import { Bash, Fish, PowerShell, WinCmd, Zsh } from "./shellcode/shells.js"
+import testNodeVersion from "./shellcode/test-node-version.js"
+import describe from "./describe.js"
+import os from "node:os"
+import { execa } from "execa"
+import path from "node:path"
+
+if (os.platform() === "win32") {
+  beforeAll(async () => {
+    // Create a scoop shim for tests
+    await execa(`scoop`, [
+      "shim",
+      "add",
+      "fnm_release",
+      path.join(__dirname, "../target/release/fnm.exe"),
+    ])
+  })
+
+  for (const shell of [Bash, Zsh, Fish, PowerShell, WinCmd]) {
+    describe(shell, () => {
+      test(`scoop shims infer the shell`, async () => {
+        await script(shell)
+          .then(shell.env({ executableName: "fnm_release" }))
+          .then(shell.call("fnm_release", ["install", "v20.14.0"]))
+          .then(shell.call("fnm_release", ["use", "v20.14.0"]))
+          .then(testNodeVersion(shell, "v20.14.0"))
+          .execute(shell)
+      })
+    })
+  }
+}
