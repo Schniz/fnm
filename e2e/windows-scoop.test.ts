d0e58aff3ffeb44326c3c0dafef7034c9f7815e7
Gal Schlezinger <gal@spitfire.co.il>
2024-06-13 13:10:32 +0300

    skip instead of not emitting tests

--- e2e/windows-scoop.test.ts
+++ e2e/windows-scoop.test.ts
@@ -5,6 +5,8 @@ import describe from "./describe.js"
 import os from "node:os"
 import { execa } from "execa"
 
+const testIf = (b: boolean) => (b ? test : test.skip)
+
 if (os.platform() === "win32") {
   beforeAll(async () => {
     // Create a scoop shim for tests
@@ -18,14 +20,17 @@ if (os.platform() === "win32") {
 
   for (const shell of [Bash, Zsh, Fish, PowerShell, WinCmd]) {
     describe(shell, () => {
-      test(`scoop shims infer the shell`, async () => {
-        await script(shell)
-          .then(shell.env({ executableName: "fnm_release" }))
-          .then(shell.call("fnm_release", ["install", "v20.14.0"]))
-          .then(shell.call("fnm_release", ["use", "v20.14.0"]))
-          .then(testNodeVersion(shell, "v20.14.0"))
-          .execute(shell)
-      })
+      testIf(os.platform() === "win32")(
+        `scoop shims infer the shell`,
+        async () => {
+          await script(shell)
+            .then(shell.env({ executableName: "fnm_release" }))
+            .then(shell.call("fnm_release", ["install", "v20.14.0"]))
+            .then(shell.call("fnm_release", ["use", "v20.14.0"]))
+            .then(testNodeVersion(shell, "v20.14.0"))
+            .execute(shell)
+        },
+      )
     })
   }
 }
