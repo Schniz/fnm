8d0c1e8d61018494bd0f6e4e024bedae82166402
Gal Schlezinger <gal@spitfire.co.il>
2024-06-13 13:21:29 +0300

    use test.skip

--- e2e/windows-scoop.test.ts
+++ e2e/windows-scoop.test.ts
@@ -5,9 +5,9 @@ import describe from "./describe.js"
 import os from "node:os"
 import { execa } from "execa"
 
-const testIf = (b: boolean) => (b ? test : test.skip)
-
-if (os.platform() === "win32") {
+if (os.platform() !== "win32") {
+  test.skip("scoop shims only work on Windows", () => {})
+} else {
   beforeAll(async () => {
     // Create a scoop shim for tests
     await execa(`scoop`, [
@@ -20,17 +20,14 @@ if (os.platform() === "win32") {
 
   for (const shell of [Bash, Zsh, Fish, PowerShell, WinCmd]) {
     describe(shell, () => {
-      testIf(os.platform() === "win32")(
-        `scoop shims infer the shell`,
-        async () => {
-          await script(shell)
-            .then(shell.env({ executableName: "fnm_release" }))
-            .then(shell.call("fnm_release", ["install", "v20.14.0"]))
-            .then(shell.call("fnm_release", ["use", "v20.14.0"]))
-            .then(testNodeVersion(shell, "v20.14.0"))
-            .execute(shell)
-        },
-      )
+      test(`scoop shims infer the shell`, async () => {
+        await script(shell)
+          .then(shell.env({ executableName: "fnm_release" }))
+          .then(shell.call("fnm_release", ["install", "v20.14.0"]))
+          .then(shell.call("fnm_release", ["use", "v20.14.0"]))
+          .then(testNodeVersion(shell, "v20.14.0"))
+          .execute(shell)
+      })
     })
   }
 }
