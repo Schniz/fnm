b90d4e927e3a50c2e8c72103492d5b9b2d2aa50d
Gal Schlezinger <gal@spitfire.co.il>
2024-10-06 14:48:13 +0300

    don't have obsolete snapshots

--- e2e/old-versions.test.ts
+++ e2e/old-versions.test.ts
@@ -7,18 +7,18 @@ import os from "node:os"
 for (const shell of [Bash, Zsh, Fish, PowerShell, WinCmd]) {
   describe(shell, () => {
     test(`download old Node.js 0.10.x`, async () => {
-      if (os.platform() === "win32") {
-        console.warn(`test skipped as 0.10.x isn't prebuilt for windows`)
-        return
-      }
-
-      await script(shell)
+      const testCase = script(shell)
         .then(shell.env({}))
         .then(shell.call("fnm", ["install", "v0.10.36"]))
         .then(shell.call("fnm", ["use", "v0.10.36"]))
         .then(testNodeVersion(shell, "v0.10.36"))
         .takeSnapshot(shell)
-        .execute(shell)
+
+      if (os.platform() === "win32") {
+        console.warn(`test skipped as 0.10.x isn't prebuilt for windows`)
+      } else {
+        await testCase.execute(shell)
+      }
     })
   })
 }
